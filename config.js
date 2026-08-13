718c2742a5cdfe505f2f1698cc5008b3b9d4f3e2
alabykhan-lang <alabykhan@gmail.com>
2026-08-13 14:41:08 +0100

    Add current-origin portal SSO configuration

--- config.js
+++ config.js
@@ -1,1 +1,11 @@
-"use strict";window.WTS_CONFIG=Object.freeze({supabaseUrl:"https://wuftzyeajmsxdrbwaawl.supabase.co",publishableKey:["sb","publishable","7AKtP6jh9xg8CdrK8F53xA","q4yZskPJ"].join("_")});
+"use strict";
+const portalOrigin=String(window.WTS_PORTAL_ORIGIN||"https://wts-school-platform.vercel.app").replace(/\/$/,"");
+const notificationOrigin=window.location.origin.replace(/\/$/,"");
+window.WTS_CONFIG=Object.freeze({
+  supabaseUrl:"https://wuftzyeajmsxdrbwaawl.supabase.co",
+  publishableKey:["sb","publishable","7AKtP6jh9xg8CdrK8F53xA","q4yZskPJ"].join("_"),
+  portalOrigin,
+  authorizeUri:portalOrigin+"/api/sso/authorize",
+  notificationOrigin,
+  redirectUri:notificationOrigin+"/",
+});
